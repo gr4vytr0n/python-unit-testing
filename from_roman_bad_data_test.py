669afd69df8b5a5df49163016d78ea47086b5144
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-13 07:53:41 -0800

    finished up unit testing exercises at this point

--- from_roman_bad_data_test.py
+++ from_roman_bad_data_test.py
@@ -5,6 +5,20 @@ import unittest
 class BadValues(unittest.TestCase):
   # test with invalid Roman numerals
   def test_too_many_repeated_characters(self):
-    '''test should fail when input value contains too many repeating characters'''
+    '''expception should be raised when input value contains too many repeating characters'''
     for s in ('MMMM', 'DD', 'CCCC', 'LL', 'XXXX', 'VV', 'IIII'):
       self.assertRaises(InvalidRomanNumeralError, from_roman, s)
+
+  def test_bad_repeated_pairs(self):
+    '''expception should be raised when input value contains bad repeated pairs of characters'''
+    for s in ('CMCM', 'CDCD', 'XCXC', 'XLXL', 'IXIX', 'IVIV'):
+      self.assertRaises(InvalidRomanNumeralError, from_roman, s)
+
+  def test_from_roman_malformed_antecedents(self):
+    '''expception should be raised when input value contains a malformed antecedent'''
+    for s in ('IIMXCC', 'VX', 'DCM', 'CMM', 'IXIV', 'MCMC', 'XCX', 'IVI', 'LM', 'LD', 'LC'):
+      self.assertRaises(InvalidRomanNumeralError, from_roman, s)
+
+  def test_from_roman_empty_string_input_value(self):
+    '''expception should be raised when input value is an empty string'''
+    self.assertRaises(InvalidRomanNumeralError, from_roman, '')
